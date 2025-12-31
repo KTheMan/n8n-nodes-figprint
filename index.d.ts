da2590c3f9e26598f53357cb8a5264d70f7a9499
KTheMan <k@knnygrdn.com>
2025-12-30 19:14:22 -0800

    Add initial implementation of Figprint node with version update to 1.0.18 and include type definitions

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+	n8n: {
+		n8nNodesApiVersion: 1;
+	};
+};
+
+export = _default;
---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+	n8n: {
+		n8nNodesApiVersion: 1,
+	},
+};
